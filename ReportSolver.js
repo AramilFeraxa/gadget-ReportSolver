78a25429839c9c5ba566406a57b8e6c7b9dc4507
AramilFeraxa <AramilFeraxa@fake.wikipedia.org>
2025-02-27 15:26:06 +0000

    fix

--- ReportSolver.js
+++ ReportSolver.js
@@ -330,7 +330,7 @@ $(function () {
                 section: sectionNumber
             }).done(function (result) {
                 var wikitext = result.parse.wikitext['*'];
-                wikitext = wikitext.replace(/\{\{\s*Status\s*\|\s*[^\|\}]*\s*\}\}/g, '{{Status|' + status + '}}');
+				wikitext = wikitext.replace(/\{\{\s*Status\s*(?:\|[^}]*)?\}\}/g, '{{Status|' + status + '}}');
                 var isCloseAction = (editSummary === 'Closed');
                 comment = comment.trim();
                 if (!comment.endsWith('.')) {
